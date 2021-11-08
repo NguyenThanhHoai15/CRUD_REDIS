8ba6cfb4dfe12568e3f1f5882d25fa8d34d21451
Nguyễn Thanh Hoài <73808149+NguyenThanhHoai15@users.noreply.github.com>
2021-11-08 11:39:55 +0700

    Add files via upload

--- src/main/java/com/example/crud_new/repository/EmployeeRepository.java
+++ src/main/java/com/example/crud_new/repository/EmployeeRepository.java
@@ -4,9 +4,11 @@ import com.example.crud_new.model.Employee;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class EmployeeRepository {
@@ -14,28 +16,33 @@ public class EmployeeRepository {
     private HashOperations hashOperations;//crud hash
     private RedisTemplate redisTemplate;
     private ListOperations listOperations;
+    private SetOperations setOperations;
 
     public EmployeeRepository(RedisTemplate redisTemplate) {
 
-        this.hashOperations = redisTemplate.opsForHash();
+//        this.hashOperations = redisTemplate.opsForHash();
         this.redisTemplate = redisTemplate;
-        this.listOperations = redisTemplate.opsForList();
+//        this.listOperations = redisTemplate.opsForList();
+        this.setOperations = redisTemplate.opsForSet();
 
     }
 
     public void saveEmployee(Employee employee){
 
-        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+//        hashOperations.put("EMPLOYEE", employee.getId(), employee);
+        setOperations.add("EMPLOYEE", employee);
     }
 
-    public List<Employee> findAll(){
+    public Set<Employee> findAll(){
 
-        return hashOperations.values("EMPLOYEE");
+        //return hashOperations.values("EMPLOYEE");
+        return setOperations.members("EMPLOYEE");
     }
 
     public Employee findById(Integer id){
 
-        return (Employee) hashOperations.get("EMPLOYEE", id);
+//        return (Employee) hashOperations.get("EMPLOYEE", id);
+        return (Employee) setOperations.intersect("EMPLOYEE", id);
     }
 
     public void update(Employee employee){
@@ -43,6 +50,7 @@ public class EmployeeRepository {
     }
 
     public void delete(Integer id){
-        hashOperations.delete("EMPLOYEE", id);
+        setOperations.remove("EMPLOYEE", id);
+//        hashOperations.delete("EMPLOYEE", id);
     }
 }
